6a9719e585eb35203c95f678911d50c67f20857c
CaioFerreira <prozcaio@gmail.com>
2024-09-12 09:59:07 -0300

    debug script.js, i found and solved it

--- script.js
+++ script.js
@@ -16,4 +16,9 @@ const user = {
     }
   }
 
-console.log(user.friends.address.complement)
+// console.log(user.friends.address.complement) //Fazendo dessa forma, o erro acontece pois não é possivel
+                                                // ler propriedades que não existem
+// para que esse erro não ocorra podemos utilizar o encadiador opcional, dessa forma:
+console.log(user?.friends?.address?.complement) // uma vez que utilizar a interrogação faz com que o JS valide
+                                                // se aquilo que ele está lendo é null ou undefined antes de 
+                                                // tentar ler a propriedade
