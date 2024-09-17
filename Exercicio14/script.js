9eb7d5a10f00684128ade416edca0f8482ef40e4
CaioFerreira <prozcaio@gmail.com>
2024-09-17 10:39:24 -0300

    Created a new function to calculate the median

--- Exercicio14/script.js
+++ Exercicio14/script.js
@@ -14,9 +14,23 @@ pondMedia = (array) => {
     return sumVal / sumWg
 }
 
+mediana = (array) => {
+    array.sort((a, b) => a - b)
+    let midtest = array.length % 2
+
+    if(midtest === 1){
+        let mid = (array.length - 1) / 2
+        return array[mid]
+    }else{
+        let mid = array.length / 2
+        return ariMedia([array[mid - 1], array[mid]])
+    }
+}
+
 let number = [10,20,30,10,20,20]
 
 let number2 = [[7,2],[9,5],[3,1]]
 
 console.log(ariMedia(number))
 console.log(pondMedia(number2))
+console.log(mediana([15, 14, 8, 7, 3]))
